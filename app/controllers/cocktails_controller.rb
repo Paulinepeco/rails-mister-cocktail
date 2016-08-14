5c56af8b3c0bad7dace031b295d14a6b138fc17c
Pauline Pecorini <pauline.pecorini@gmail.com>
2016-08-14 18:57:26 +0200

    avant modif

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,6 +21,22 @@ class CocktailsController < ApplicationController
     redirect_to cocktail_path(@cocktail)
   end
 
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.delete
+    redirect_to root_path
+  end
+
   private
 
   def cocktail_params
